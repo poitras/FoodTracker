5f371951f625325252cec20c33954cc554cbfc9c
poitras <nicopoitras@gmail.com>
2017-03-20 23:36:12 -0400

    Button action created

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,11 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK: Action
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Hello World"
+    }
+    
+    
 
 }
 
